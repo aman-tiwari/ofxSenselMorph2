60602c9b4b40d4ed3476f45423676cf6644c575c
Aman Tiwari <aman26@gmail.com>
2017-03-28 17:09:43 -0400

    fixes #1 , addds scaling

--- example-basic/src/ofApp.cpp
+++ example-basic/src/ofApp.cpp
@@ -1,16 +1,20 @@
 #include "ofApp.h"
+#include "ofxSenselMorph.h"
 
 ofxSenselMorph sensel;
+
+float log_scale_top = 5.0f;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetColor(0);
-    
+    ofSetFrameRate(60);
     ofSetBackgroundColor(0);
     ofFill();
 
     ofSetWindowShape(ofGetWindowWidth(), (int)ofGetWindowWidth() *
                      ((float)sensel.rows() / (float)sensel.cols()));
+    
 }
 
 
@@ -25,34 +29,48 @@ void ofApp::update(){
 void ofApp::draw(){
     auto rows = sensel.rows();
     auto cols = sensel.cols();
-    const auto&& forces = sensel.forces();
+    const auto& forces = sensel.forces();
     
     auto scale_factor = ofGetWindowWidth() / (float) cols;
     
     ofPushMatrix();
     {
-        ofScale(scale_factor);
+        ofScale(scale_factor, scale_factor);
         
         for(size_t row = 0; row < rows; row++) {
             for(size_t col = 0; col < cols; col++) {
                 
                 const auto f_xy_raw = forces[row * cols + col];
                 
-                float g = ofMap(log(f_xy_raw + 1.0)/log(10), 0, 5, 0, 255);
+                float g = ofMap(log(f_xy_raw + 1.0)/log(10), 0,
+                                log_scale_top, 0, 255);
                 
                 g = ofClamp(g, 0, 255);
                 
-                ofSetColor(g,g,g);
+                ofSetColor(g, g, g);
                 ofDrawRectangle(col, row, 1, 1);
             }
         }
     }
     ofPopMatrix();
+    
+    float max_force = *std::max_element(forces.begin(), forces.end());
+
+    std::cout << max_force << '\n';
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    switch(key) {
+            case '<':
+            case ',':
+            log_scale_top = std::max(1.0f, log_scale_top - 1);
+            break;
+            case '>':
+            case '.':
+            log_scale_top = std::min(6.0f, log_scale_top + 1);
+            break;
+    }
 }
 
 //--------------------------------------------------------------
